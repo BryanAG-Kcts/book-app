58c64901b0275d502f08ffd8c9de8a0fb70e927a
BryanAG-kcts <bryanalvarezg.kcts@gmail.com>
2025-07-04 10:36:57 -0500

    feat: agregar endpoint POST para libros y validaciones con Zod

--- book-app-backend/src/controllers/book.ts
+++ book-app-backend/src/controllers/book.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { BookStorage } from '../services/book-storage'
+import { BookZodSchema } from '../interfaces/book'
 
 export const bookController = {
   getBooks(_req: Request, res: Response) {
@@ -7,6 +8,28 @@ export const bookController = {
       message: 'Get books',
       status: 200,
       data: BookStorage.getBooks()
+    })
+  },
+  async postBook(req: Request, res: Response) {
+    const rawBook = req.body
+    const result = await BookZodSchema.safeParseAsync(rawBook)
+    if (!result.success) {
+      res.status(400).json({
+        message: 'Bad request',
+        status: 400,
+        errors: result.error.issues
+      })
+
+      return
+    }
+
+    const books = BookStorage.getBooks()
+    const book = { ...result.data, id: (books[books.length - 1].id ?? 0) + 1 }
+    BookStorage.addBook(book)
+    res.status(201).json({
+      message: 'Book created',
+      status: 201,
+      data: book
     })
   }
 }
