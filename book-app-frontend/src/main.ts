37d736db6dd955a15557ec8699ee3fd7c674ea0e
BryanAG-kcts <bryanalvarezg.kcts@gmail.com>
2025-07-07 07:46:08 -0500

    feat: Se agregó detalles de libro y modo de edición

--- book-app-frontend/src/main.ts
+++ book-app-frontend/src/main.ts
@@ -5,12 +5,14 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Material from '@primeuix/themes/material'
 import { router } from './router'
+import { ToastService } from 'primevue'
 
 const pinia = createPinia()
 const app = createApp(MainLayout)
 
 app.use(router)
 app.use(pinia)
+app.use(ToastService)
 app.use(PrimeVue, {
   theme: {
     preset: Material
--- book-app-frontend/src/pages/BookDetailPage.vue
+++ book-app-frontend/src/pages/BookDetailPage.vue
@@ -1,3 +1,51 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import TitlePage from '../components/atoms/TitlePage.vue'
+import { useBookStore } from '../store/books'
+import { useRoute } from 'vue-router'
+import { Message } from 'primevue'
+import BookForm from '../components/organisms/BookForm.vue'
+import type { Book } from '../interfaces/book'
+
+const route = useRoute()
+const bookStore = useBookStore()
+onMounted(() => {
+  bookStore.getBookById(Number(route.params.id as string))
+})
+
+function handleSubmit(book: Book) {
+  bookStore.updateBook(book)
+}
+</script>
+
 <template>
-    book-detail
+  <Message
+    severity="info"
+    icon="pi pi-spin pi-spinner"
+    v-if="bookStore.loading"
+  >
+    Cargando libro
+  </Message>
+
+  <Message
+    severity="error"
+    icon="pi pi-times-circle"
+    v-if="!bookStore.selectedBook && !bookStore.loading"
+  >
+    No se encontró el libro solicitado
+  </Message>
+
+  <div
+    v-else
+    class="flex flex-col gap-3"
+  >
+    <TitlePage :title="`Detalles de ` + bookStore.selectedBook?.title">
+      <i
+        class="pi pi-file"
+        style="font-size: 1.875rem"
+      ></i>
+    </TitlePage>
+
+    <BookForm :book="bookStore.selectedBook ?? undefined" @handleSubmit="handleSubmit"></BookForm>
+  </div>
 </template>
